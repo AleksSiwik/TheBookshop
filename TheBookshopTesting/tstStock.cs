7f4e037b465bb0d423cf458c0a63506964061247
Yasin Patel <P2437884@my365.dmu.ac.uk>
2020-03-20 15:23:50 +0000

    added testing and class for stock m

---
+++ TheBookshopTesting/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheBookshopTesting
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStock AStock = new clsStock();
+
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
